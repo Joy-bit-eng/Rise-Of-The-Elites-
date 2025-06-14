cc533eaa74d096babbd43936fced16d890801548
Joy-bit-eng <ls9988236@gmail.com>
2025-06-14 18:30:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,8 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/Rise-Of-The-Elites-/', // 👈 VERY important!
+  base: '/Rise-Of-The-Elites--/', // use your GitHub repo name
   build: {
-    outDir: 'dist',
-  },
-});
-  plugins: [VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.ico'],
-    manifest: {
-      name: 'Rise of the Elites',
-      short_name: 'Elites',
-      start_url: './index.html',
-      display: 'fullscreen',
-      background_color: '#1d1d1d',
-      theme_color: '#00ffcc',
-      icons: [
-        {
-          src: '/assets/icons/icon-192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/assets/icons/icon-512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    }
-  })],
+    outDir: 'dist'
+  }
 });
