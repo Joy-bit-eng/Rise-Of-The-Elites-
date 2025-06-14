6572ba85a1668258ba97470fe0aa6a2f04f5bef7
Joy-bit-eng <ls9988236@gmail.com>
2025-06-14 18:32:27 +0530

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -4,24 +4,9 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
-    const map = this.make.tilemap({ key: 'world' });
-    const tileset = map.addTilesetImage('tileset', 'tiles');
-    map.createLayer('Ground', tileset);
-    map.createLayer('Props', tileset);
-
-    this.player = this.physics.add.sprite(100, 100, 'player', 0);
-    this.cursors = this.input.keyboard.createCursorKeys();
-  }
-
-  update() {
-    const speed = 150;
-    const { left, right, up, down } = this.cursors;
-    this.player.setVelocity(0);
-
-    if (left.isDown) this.player.setVelocityX(-speed);
-    else if (right.isDown) this.player.setVelocityX(speed);
-
-    if (up.isDown) this.player.setVelocityY(-speed);
-    else if (down.isDown) this.player.setVelocityY(speed);
+    this.add.text(100, 100, 'Rise of the Elites!', {
+      fontSize: '32px',
+      fill: '#ffffff'
+    });
   }
 }
